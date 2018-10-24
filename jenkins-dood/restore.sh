10a1d0f3dd5a2f99d7941f44d2d08802f2bb82ff
Adrian Herrera <simplyadrian@gmail.com>
2018-10-24 16:36:43 +0000

    adding environment vars

--- jenkins-dood/restore.sh
+++ jenkins-dood/restore.sh
@@ -1,6 +1,6 @@
 if [[ -n "$PRODUCT" ]] && [[ ! -e $JENKINS_HOME/config.xml ]]
 then
-  aws --region $REGION s3 cp "s3://$ROOT_BUCKET/$PRODUCT/jenkins/jenkins_data.tgz" /tmp/restored.tgz &&\
+  aws --region $REGION s3 cp "s3://$ROOT_BUCKET/$PRODUCT/jenkins_data.tgz" /tmp/restored.tgz &&\
   tar -xzf /tmp/restored.tgz -C /  &&\
   chown -R jenkins:jenkins $JENKINS_HOME &&\
   rm /tmp/restored.tgz || true
